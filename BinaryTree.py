f6f97fd472cfd3909de358fb5b297d016e7cd650
Stupnikov Dmitry <d_lis82@mail.ru>
2025-05-27 13:15:02 +0300

    дерево, ширина

---
+++ BinaryTree.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self._insert_recursive(self.root, value)
+
+    def _insert_recursive(self, current_node, value):
+        if value < current_node.value:
+            if current_node.left is None:
+                current_node.left = Node(value)
+            else:
+                self._insert_recursive(current_node.left, value)
+        else:
+            if current_node.right is None:
+                current_node.right = Node(value)
+            else:
+                self._insert_recursive(current_node.right, value)
+
+    def search(self, value):
+        return self._search_recursive(self.root, value)
+
+    def _search_recursive(self, current_node, value):
+        if current_node is None:
+            return False
+        if current_node.value == value:
+            return True
+        elif value < current_node.value:
+            return self._search_recursive(current_node.left, value)
+        else:
+            return self._search_recursive(current_node.right, value)
+
+    def inorder(self, node):
+        if node:
+            self.inorder(node.left)
+            print(node.value, end=' ')
+            self.inorder(node.right)
+
+
+
+
+bt = BinaryTree()
+bt.insert(5)
+bt.insert(3)
+bt.insert(7)
+
+bt.inorder(bt.root)
+
+print(bt.search(5))
+print(bt.search(10))
---
+++ BreadthFirstSearch.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+
+class Node:
+    def __init__(self, key):
+        self.val = key
+        self.left = None
+        self.right = None
+def breadth_first_traversal(root):
+    if root is None:
+        return
+    queue = deque([root])
+    while queue:
+        node = queue.popleft()
+        print(node.val, end = '')
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+
+
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(2)
+root.left.left = Node(3)
+root.right.right = Node(4)
+root.left.right = Node(5)
+root.right.left = Node(6)
+
+breadth_first_traversal(root)
+
+
